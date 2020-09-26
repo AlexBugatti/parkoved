228e41f322cdce5f63210f8ee0b7bbc7c0e9da51
Александр <buga-91@inbox.ru>
2020-09-26 10:22:47 +0600

    initial commit

---
+++ Parkoved/Parkoved/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  Parkoved
+//
+//  Created by Александр on 26.09.2020.
+//  Copyright © 2020 AlexBugatti. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+}
+
